4979b4907dc3ae0acc2f002aada37ca0bd5260bc
McDodgerman <59996045+McDodgerman@users.noreply.github.com>
2022-10-13 16:43:05 -0500

    initial commit
    Android Basics in Kotlin, Unit 3: Navigation
    This application was used to explore lifecycles as used by activities, and the use of intents to open the Internet browser.
    Later in Unit 5: Data Persistence, Jetpack DataStore was used to store a layout setting.

--- app/src/main/java/com/stargrazer/wordsapp/MainActivity.kt
+++ app/src/main/java/com/stargrazer/wordsapp/MainActivity.kt
@@ -13,30 +13,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.wordsapp
+package com.stargrazer.wordsapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.example.wordsapp.databinding.ActivityMainBinding
+import com.stargrazer.wordsapp.databinding.ActivityMainBinding
 
 /**
  * Main Activity and entry point for the app. Displays a RecyclerView of letters.
  */
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     private lateinit var recyclerView: RecyclerView
+    private var isLinearLayoutManager = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        recyclerView = binding.recyclerView
-        // Sets the LinearLayoutManager of the recyclerview
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = LetterAdapter()
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
